96bb303d8f0bafd33f14d54cdb8762bdad420e03
Tony (Chirawat Koeichai) <tonezzz@hotmail.com>
2026-03-05 13:10:57 +0700

    Updated the autogen.bat script to enhance recursive scanning for generating INDEX.md

--- projects/noble-a/autogen.bat
+++ projects/noble-a/autogen.bat
@@ -1,21 +1,53 @@
 @echo off
 
+REM Initialize Variables
 setlocal enabledelayedexpansion
+set "dir_tree="
+set "file_count=0"
+set "pdf_count=0"
+set "xlsx_count=0"
+set "docx_count=0"
 
-REM Enhanced recursive subdirectory scanning and detailed file information generation
-
-set "root_dir=%cd%"
-
-echo Scanning directories in: !root_dir!
-
-echo ---------------------------------------
-
-REM Get a list of all files in the current directory and subdirectories
-for /r %%f in (*) do (
-    echo File: %%f
-    echo Size: %%~zf bytes
-    echo Last Modified: %%~tf
-    echo ---------------------------------------
+REM Function to recursively parse directories
+:parse_dir
+for %%F in (.) do (
+    echo %%~nxF
+    set "dir_tree=!dir_tree!\%%~nxF"
+    set /a file_count+=1
+    for /r %%F %%G in (*.pdf) do (
+        set /a pdf_count+=1
+    )
+    for /r %%F %%G in (*.xlsx) do (
+        set /a xlsx_count+=1
+    )
+    for /r %%F %%G in (*.docx) do (
+        set /a docx_count+=1
+    )
 )
 
-pause
+REM Create INDEX.md File
+set index_file=INDEX.md
+(echo # Table of Contents
+echo 
+) > !index_file!
+(echo ## Directory Structure
+echo !dir_tree! 
+) >> !index_file!
+(echo ## File Statistics
+echo Total Files: !file_count! 
+) >> !index_file!
+(echo PDF Files: !pdf_count! 
+) >> !index_file!
+(echo XLSX Files: !xlsx_count! 
+) >> !index_file!
+(echo DOCX Files: !docx_count! 
+) >> !index_file!
+
+REM Listing Detailed Information
+(echo ## Detailed File Listing) >> !index_file!
+for /r %%D in (*) do (
+    echo - %%~nxD; >> !index_file!
+)
+
+REM Finish
+endlocal
